b78543b2c5b02f8dd9be521644c6fe7707a91c36
alunos <laisspraxedes@hotmail.com>
2017-03-02 12:11:45 -0300

    exerciooaaaaaaaaa

---
+++ ep1/Lista.java
@@ -0,0 +1,37 @@
+class No {
+   private int x;
+   private No prox,ant;
+     public void setX (int x)
+     {
+          this.x=x;
+     }
+     public int getX(){
+          return this.x;
+     }
+     public void setProx (No prox)
+     {
+         this.prox=prox;
+     }
+     public No getProx ()
+     {
+         return this.prox;
+     } 
+     public void setAnt (No ant)
+     {
+          this.ant=ant;
+     }
+     public No getAnt ()
+     {
+         return this.ant;
+     }
+}
+
+    public class Lista
+{
+      public void inserir(int pos,int item){
+
+
+
+
+     }
+}
