56db4cbb134710ef564da7a5062f14e83c701a20
alunos <laisspraxedes@hotmail.com>
2017-03-13 12:25:28 -0300

    notas

---
+++ ep3/Disciplina.java
@@ -0,0 +1,30 @@
+ public class Disciplina
+ {
+  private String nome,prof;
+  public void setNome(String nome)
+    {
+      this.nome=nome; 
+    }
+    public String getNome()
+    {
+      return this.nome;
+    }
+    public void setProf (String prof)
+    {
+        this.prof=prof;
+    }
+    public String getProf ()
+   {
+        return this.prof;
+   }
+
+
+
+
+
+
+
+
+
+
+ }
---
+++ ep3/Nota.java
@@ -0,0 +1,30 @@
+ public class Nota
+ {
+    double valor;
+    Aluno a;
+    Disciplina d;
+    public void setValor(double valor)
+    {
+      this.valor=valor;
+    }
+    public double getValor()
+    {
+      return this.valor;
+    }
+    public void setA (Aluno a)
+    {
+      this.a=a;
+    }
+    public Aluno getA()
+    {
+      return this.a;
+    }
+    public void setD(Disciplina d)
+    {
+       this.d=d;
+    }
+    public Disciplina getD()
+    {
+       return this.d;
+    }
+ }
