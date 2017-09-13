177a70551a904714b75d483b0163800285c4022d
Alunos <laisspraxedes@hotmail.com>
2017-09-13 12:10:03 -0300

    ListaDinamica(correta)

--- Exercicios/ListaDinamica/app/src/main/java/com/example/alunos/listadinamica/adapter/PessoaAdapter.java
+++ Exercicios/ListaDinamica/app/src/main/java/com/example/alunos/listadinamica/adapter/PessoaAdapter.java
@@ -16,42 +16,45 @@ import java.util.List;
  * Created by alunos on 13/09/17.
  */
 
-public class PessoaAdapter extends BaseAdapter{
+public class PessoaAdapter extends BaseAdapter {
     private Context context;
     private List<Pessoa> lista;
 
- public PessoaAdapter(Context context, List<Pessoa> lista){
-     this.context = context;
-     this.lista = lista;
- }
-  public int getCount(){
-      return  lista.size();
-  }
-  public Object getItem(int posicao){
+    public PessoaAdapter(Context context, List<Pessoa> lista) {
+        this.context = context;
+        this.lista = lista;
+    }
 
-       Pessoa obj = lista.get(posicao);
-       return obj;
-  }
-  public long getItemId(int posicao){
-      return posicao;
-  }
-  public View getView(int posicao, View convertView, ViewGroup parent) {
-      Pessoa obj = lista.get(posicao);
-      LayoutInflater inflater = (LayoutInflater)
-              context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-      View v = inflater.inflate(R.layout.row_layout, null);
-      TextView textNome = (TextView)
-              v.findViewById(R.id.textNome);
-      textNome.setText(obj.getNome​());
+    public int getCount() {
 
-      TextView textTelefone = (TextView)
-              v.findViewById(R.id.textTelefone);
-      textTelefone.setText(obj.getTelefone​());
+        return lista.size();
+    }
 
-      TextView textIdade = (TextView)
-              v.findViewById(R.id.textIdade);
-      textIdade.setText(String.valueOf(obj.getIdade​()));
-      return v;
-  }
+    public Object getItem(int posicao) {
+
+        Pessoa obj = lista.get(posicao);
+        return obj;
+    }
+
+    public long getItemId(int posicao) {
+        return posicao;
+    }
+
+    public View getView(int posicao, View convertView, ViewGroup parent) {
+        Pessoa obj = lista.get(posicao);
+        LayoutInflater inflater = (LayoutInflater)
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.row_layout, null);
+
+        TextView textNome = (TextView) v.findViewById(R.id.textNome);
+        textNome.setText(obj.getNome());
+
+        TextView textTelefone = (TextView) v.findViewById(R.id.textTelefone);
+        textTelefone.setText(obj.getTelefone());
+
+        TextView textIdade = (TextView) v.findViewById(R.id.textIdade);
+        textIdade.setText(String.valueOf(obj.getIdade()));
+        return v;
+    }
 }
 
--- Exercicios/ListaDinamica/app/src/main/java/com/example/alunos/listadinamica/model/Pessoa.java
+++ Exercicios/ListaDinamica/app/src/main/java/com/example/alunos/listadinamica/model/Pessoa.java
@@ -13,22 +13,23 @@ public class Pessoa {
         this.telefone = telefone;
         this.idade = idade;
     }
-    public String getNome​() {
+    public String getNome(){
         return nome;
     }
-    public void setNome​(String nome) {
+    public void setNome(String nome){
         this.nome = nome;
     }
-    public String getTelefone​() {
+    public String getTelefone(){
         return telefone;
     }
-    public void setTelefone​(String telefone) {
+    public void setTelefone(String telefone){
         this.telefone = telefone;
     }
-    public int getIdade​() {
+    public  int getIdade(){
         return idade;
     }
-    public void setIdade​(int idade) {
-        this.idade = idade;
+    public void setIdade(int idade){
+        this.idade=idade;
     }
+
 }
